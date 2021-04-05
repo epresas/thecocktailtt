e55e81d876899ad7520493bcc60a0f471762369b
Edmundo Presas <edmundoj.presas@gmail.com>
2021-04-05 23:51:04 +0200

    feat(*): Add AD structure and Nucleus styleguide

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{background: 'red'}} className='grid'>
+      <div className='grid__row'>
+        <div style={{background: 'blue'}} className='grid__col-3 grid__col-sm-6'> 3 cols</div>
+        <div style={{background: 'yellow'}} className='grid__col-3 grid__col-sm-6'> 3 cols</div>
+        <div style={{background: 'grey'}} className='grid__col-3 grid__col-sm-6'> 3 cols</div>
+        <div style={{background: 'brown'}} className='grid__col-3'> 3 cols</div>
+      </div>
+      <div className='grid__row'>
+        <div style={{background: 'blue'}} className='grid__col-4'> 4 cols</div>
+        <div style={{background: 'yellow'}} className='grid__col-4'> 4 cols</div>
+        <div style={{background: 'grey'}} className='grid__col-4'> 4 cols</div>
+      </div>
+    </div>
+  );
+}
+
+// https://github.com/diegohaz/arc
+
+export default App;
