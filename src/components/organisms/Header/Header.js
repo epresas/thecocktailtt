e7469714afa727d58f21ecf3e1d1fbc2c5ed2132
Edmundo Presas <edmundoj.presas@gmail.com>
2021-04-08 08:49:38 +0200

    feat(*): WIP components

--- src/components/organisms/Header/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import NavBar from '../../molecules/NavBar/NavBar';
-
-const Header = (props) => {
-  return <NavBar/>
-}
-
-export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import './i18n'; // needed for bundling
 import './index.scss';
 
 import App from './components/App';
 
 const renderApp = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Suspense fallback="loading...">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Suspense>
 );
 const root = document.getElementById('root');
 
